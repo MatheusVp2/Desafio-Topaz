469775098efda16c3f372f2c08b6bb3e1a1a8063
Matheus Oliveira <teteuoliveira12@gmail.com>
2022-08-16 14:36:27 -0300

    add: path file error exceptions

---
+++ error/__init__.py
@@ -0,0 +1,9 @@
+
+class LimitTasksException(Exception):
+    """Exeption de limites de tasks permitidas"""
+    
+class LimitUmaxException(Exception):
+    """Exception de limite de umax permitidos"""
+
+class NumberOfUsersExceededException(Exception):
+    """Exceptino de numeros de usuarios permitidos por servidor"""
